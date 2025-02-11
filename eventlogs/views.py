3fed13db6194fb490e01ccf369a76d88c9153b1d
Arvind A <pingarvindforquestions@gmail.com>
2025-02-11 22:44:24 +0530

    fix: updated the query

--- eventlogs/views.py
+++ eventlogs/views.py
@@ -6,5 +6,8 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def event_log_list(request: HttpRequest):
     status = request.GET.get('status', 'active')
-    event_logs = EventLog.objects.filter(trigger__user=request.user.id, status=status).select_related('trigger')
+    event_logs = EventLog.objects.filter(
+        trigger_data__user_id=request.user.id,
+        status=status
+    ).select_related('trigger')
     return render(request, 'eventlogs/event_logs.html', {'event_logs': event_logs, 'status': status})
